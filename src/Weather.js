03035fa2375d176a970a2731a771878bf0c757cb
ChandreLesch <143355884+ChandreLesch@users.noreply.github.com>
2023-12-11 02:09:31 +0200

    Axios integrations to display city weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,26 @@
 import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
-export default function Weather() {
-  return <h2>Hello from Weather</h2>;
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} °C`
+    );
+  }
+  let apiKey = "c4b172eedcc31d3b2dcffd5a7db9d456";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
 }
